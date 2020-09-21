1dce178cb73e4a113e4c0984c59c99ee6f7a7e0e
charlesbaird-dsa <68249424+charlesbaird-dsa@users.noreply.github.com>
2020-09-21 15:57:32 +0100

    sanitised csvs

---
+++ data/process_csv.rb
@@ -0,0 +1,38 @@
+require 'csv'
+require 'FileUtils'
+require 'securerandom'
+
+CSV.open("operational.csv", "wb") do |csv|
+csv << ["dateAdded", "dateUpdated",  "uuid"]
+end
+
+CSV.open("catalogue.csv", "wb") do |csv|
+csv << ["uuid", "url", "name", "description", "documentation", "license", "maintainer", "provider", "areaServed", "startDate", "endDate", "organisation"]
+end
+
+CSV.foreach('apic.csv', headers:true).with_index(10) do |row|  #load csv file by row, include headers
+
+#generate uuid
+uuid = SecureRandom.uuid
+#generate date
+date = Time.new.strftime("%Y-%m-%d")
+
+#add dateadded, if blank
+dateadded = row['dateadded']
+if !dateadded
+  dateadded = date
+end
+#add dateupdated, if blank
+dateupdated = row['dateupdated']
+if !dateupdated
+  dateupdated = date
+end
+
+CSV.open("operational.csv", "a+") do |csv|
+csv << ["#{dateadded}", "#{dateupdated}",  "#{uuid}"]
+end
+
+CSV.open("catalogue.csv", "a+") do |csv|
+csv << ["#{uuid}", "#{row['url']}", "#{row['name']}", "#{row['description']}", "#{row['documentation']}", "#{row['license']}", "#{row['maintainer']}", "#{row['provider']}", "#{row['areaServed']}", "#{row['startDate']}", "#{row['endDate']}", "#{row['organisation']}"]
+end
+end
