863c6703ee22c45672d617e6e4ffae67a0fd7fee
Oliver Peate <oliverp@gmail.com>
2020-09-25 15:54:36 +0100

    Dynamically generate pages from CSV data
    Organisation index & API detail pages are now generated from a CSV
    file, using Middleman's proxy feature for dynamic pages:
    
    https://middlemanapp.com/advanced/dynamic-pages/

--- config.rb
+++ config.rb
@@ -1,7 +1,30 @@
 require 'govuk_tech_docs'
+require 'lib/api_catalogue'
 
 GovukTechDocs.configure(self)
 
 # use relative paths for links and sources
 activate :relative_assets
 set :relative_links, true
+
+csv_path = File.expand_path("data/inputs/apic.csv", __dir__)
+
+ApiCatalogue.from_csv(csv_path).organisations_apis.each do |organisation, apis|
+  proxy(
+    "/#{organisation.slug}/index.html",
+    "organisation_index.html",
+    locals: { organisation: organisation },
+    data: { title: organisation.name },
+    ignore: true,
+  )
+
+  apis.each do |api|
+    proxy(
+      "/#{organisation.slug}/#{api.slug}/index.html",
+      "api_details.html",
+      locals: { api: api },
+      data: { title: api.name },
+      ignore: true,
+    )
+  end
+end
