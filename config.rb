28603bd0e27eb74a1972d29486237de980a6a24e
Oliver Peate <oli@shippedcode.com>
2020-10-01 15:28:50 +0100

    Order orgs & APIs in ToC from A-Z

--- config.rb
+++ config.rb
@@ -14,21 +14,31 @@ helpers UrlHelpers
 csv_path = File.expand_path("data/inputs/apic.csv", __dir__)
 api_catalogue = ApiCatalogue.from_csv(csv_path)
 
-api_catalogue.organisations_apis.each do |organisation, apis|
+# Order organisations from A-Z in the Table of Contents,
+# leaving a buffer from 0-999 for static content to be given priority
+initial_org_weight = 1_000
+
+api_catalogue.organisations_apis.each.with_index(initial_org_weight) do |(organisation, apis), org_weight|
   proxy(
     UrlHelpers.organisation_path(organisation),
     "organisation_index.html",
     locals: { organisation: organisation, apis: apis },
-    data: { title: organisation.name },
+    data: {
+      title: organisation.name,
+      weight: org_weight,
+    },
     ignore: true,
   )
 
-  apis.each do |api|
+  apis.each_with_index do |api, api_weight|
     proxy(
       UrlHelpers.api_path(organisation: organisation, api: api),
       "api_details.html",
       locals: { api: api },
-      data: { title: api.name },
+      data: {
+        title: api.name,
+        weight: api_weight,
+      },
       ignore: true,
     )
   end
--- spec/lib/api_catalogue_spec.rb
+++ spec/lib/api_catalogue_spec.rb
@@ -1,30 +1,49 @@
 require "api_catalogue"
 
-RSpec.describe ApiCatalogue, "::from_csv" do
-  let(:csv_path) { File.expand_path("../../data/inputs/apic.csv", __dir__) }
+RSpec.describe ApiCatalogue do
+  describe "::from_csv" do
+    let(:csv_path) { File.expand_path("../../data/inputs/apic.csv", __dir__) }
 
-  it "parses the CSV source" do
-    api_catalogue = described_class.from_csv(csv_path)
+    it "parses the CSV source" do
+      api_catalogue = described_class.from_csv(csv_path)
 
-    expect(api_catalogue.organisations_apis.size).to be > 10
+      expect(api_catalogue.organisations_apis.size).to be > 10
 
-    gds, gds_apis = api_catalogue.organisations_apis.detect do |org, _|
-      org.name.casecmp?("Government Digital Service")
+      gds, gds_apis = api_catalogue.organisations_apis.detect do |org, _|
+        org.name.casecmp?("Government Digital Service")
+      end
+
+      notify_api = gds_apis.detect do |api|
+        api.name.casecmp?("GOV.UK Notify")
+      end
+
+      expect(gds).to have_attributes(
+        name: "Government Digital Service",
+        alternate_name: "GDS",
+      )
+
+      expect(notify_api).to have_attributes(
+        name: "GOV.UK Notify",
+        description: be_present,
+        url: "https://api.notifications.service.gov.uk/",
+      )
     end
+  end
 
-    notify_api = gds_apis.detect do |api|
-      api.name.casecmp?("GOV.UK Notify")
+  describe "ordering" do
+    it "sorts organisations and APIs from A-Z" do
+      apis = [
+        instance_double(Api, organisation: "B", name: "B2", provider: nil),
+        instance_double(Api, organisation: "B", name: "B1", provider: nil),
+        instance_double(Api, organisation: "A", name: "A2", provider: nil),
+        instance_double(Api, organisation: "A", name: "A1", provider: nil),
+      ]
+
+      api_catalogue = described_class.new(apis)
+      first_org, first_org_apis = api_catalogue.organisations_apis.first
+
+      expect(first_org.name).to eq "A"
+      expect(first_org_apis.map(&:name)).to eq ["A1", "A2"]
     end
-
-    expect(gds).to have_attributes(
-      name: "Government Digital Service",
-      alternate_name: "GDS",
-    )
-
-    expect(notify_api).to have_attributes(
-      name: "GOV.UK Notify",
-      description: be_present,
-      url: "https://api.notifications.service.gov.uk/",
-    )
   end
 end
