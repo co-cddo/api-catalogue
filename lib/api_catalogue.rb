f888c2c67914a4e0d5c9c4137cf6a5c066011e48
Oliver Peate <oli@shippedcode.com>
2020-10-01 15:28:50 +0100

    Order orgs & APIs in ToC from A-Z

--- lib/api_catalogue.rb
+++ lib/api_catalogue.rb
@@ -6,25 +6,28 @@ class ApiCatalogue
   def self.from_csv(csv_path)
     header_converter = lambda { |header| header.underscore.to_sym }
 
-    data = CSV
+    apis = CSV
       .foreach(csv_path, headers: true, header_converters: [header_converter])
       .map { |row| Api.new(row.to_hash) }
+
+    new(apis)
+  end
+
+  attr_reader :organisations_apis
+
+  def initialize(apis)
+    @organisations_apis = group_by_organisation(apis)
+  end
+
+  private
+
+  def group_by_organisation(apis)
+    apis
+      .sort_by(&:organisation)
       .group_by(&:organisation)
       .each_with_object({}) do |(organisation, apis), result|
         organisation = Organisation.new(name: organisation, alternate_name: apis.first&.provider)
         result[organisation] = apis.sort_by(&:name)
       end
-
-    new(data)
   end
-
-  attr_reader :organisations_apis
-
-  def initialize(organisations_apis)
-    @organisations_apis = organisations_apis
-  end
-
-  private
-
-  attr_reader :data
 end
