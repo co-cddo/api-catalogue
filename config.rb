0248ee405ba048a068192d40028457fdc2c49efa
Oliver Peate <oli@shippedcode.com>
2020-11-11 18:01:18 +0000

    Inline Middleman config from Tech Docs gem

--- config.rb
+++ config.rb
@@ -1,10 +1,120 @@
-require "govuk_tech_docs"
 require "lib/api_catalogue_overview"
 require "lib/api_catalogue"
 require "lib/dashboard_stats"
 require "lib/url_helpers"
 
-GovukTechDocs.configure(self)
+### Config from tech-docs-gem: start ###
+require "middleman"
+require "middleman-autoprefixer"
+require "middleman-sprockets"
+require "middleman-livereload"
+require "middleman-syntax"
+require "middleman-search"
+
+require "nokogiri"
+require "chronic"
+require "active_support/all"
+
+require "lib/govuk_tech_docs/redirects"
+require "lib/govuk_tech_docs/table_of_contents/helpers"
+require "lib/govuk_tech_docs/contribution_banner"
+require "lib/govuk_tech_docs/meta_tags"
+require "lib/govuk_tech_docs/page_review"
+require "lib/govuk_tech_docs/pages"
+require "lib/govuk_tech_docs/tech_docs_html_renderer"
+require "lib/govuk_tech_docs/unique_identifier_extension"
+require "lib/govuk_tech_docs/unique_identifier_generator"
+require "lib/govuk_tech_docs/warning_text_extension"
+require "lib/govuk_tech_docs/api_reference/api_reference_extension"
+
+activate :sprockets
+
+sprockets.append_path File.expand_path("node_modules/govuk-frontend/", __dir__)
+
+activate :syntax
+
+files.watch :source, path: File.expand_path("source", __dir__)
+
+set :markdown_engine, :redcarpet
+set :markdown,
+            renderer: GovukTechDocs::TechDocsHTMLRenderer.new(
+              with_toc_data: true,
+              api: true,
+              context: self,
+            ),
+            fenced_code_blocks: true,
+            tables: true,
+            no_intra_emphasis: true
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  activate :autoprefixer
+  activate :minify_javascript, ignore: ["/raw_assets/*"]
+end
+
+config_file = ENV.fetch("CONFIG_FILE", "config/tech-docs.yml")
+config[:tech_docs] = YAML.load_file(config_file).with_indifferent_access
+activate :unique_identifier
+activate :warning_text
+activate :api_reference
+
+helpers do
+  include GovukTechDocs::TableOfContents::Helpers
+  include GovukTechDocs::ContributionBanner
+
+  def meta_tags
+    @meta_tags ||= GovukTechDocs::MetaTags.new(config, current_page)
+  end
+
+  def current_page_review
+    @current_page_review ||= GovukTechDocs::PageReview.new(current_page, config)
+  end
+
+  def format_date(date)
+    date.strftime("%-e %B %Y")
+  end
+
+  def active_page(page_path)
+    [
+      page_path == "/" && current_page.path == "index.html",
+      ("/" + current_page.path) == page_path,
+      current_page.data.parent != nil && current_page.data.parent.to_s == page_path,
+    ].any?
+  end
+end
+
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
+
+ready do
+  redirects = GovukTechDocs::Redirects.new(self).redirects
+
+  redirects.each do |from, to|
+    redirect from, to
+  end
+end
+
+if config[:tech_docs][:enable_search]
+  activate :search do |search|
+    search.resources = [""]
+
+    search.fields = {
+      title:   { boost: 100, store: true, required: true },
+      content: { boost: 50, store: true },
+      url:     { index: false, store: true },
+    }
+
+    search.pipeline_remove = %w[stopWordFilter]
+
+    search.tokenizer_separator = '/[\s\-/]+/'
+  end
+end
+### Config from tech-docs-gem: end ###
 
 set(:layout, :api_catalogue)
 
