05436848c644a47fd5fe05e0d171bcc11ae4c9e9
Rob Nichols <robert.nichols@digital.cabinet-office.gov.uk>
2024-02-21 16:49:16 +0000

    Working

--- source/javascripts/govuk_tech_docs.js
+++ source/javascripts/govuk_tech_docs.js
@@ -3,9 +3,9 @@
 //= require _vendor/fixedsticky
 //= require lodash
 //= require _start-modules
-//= require govuk-frontend/govuk/all.js
+//= require govuk-frontend/dist/govuk/all.bundle.js
 
-$(function () {
-  $('.fixedsticky').fixedsticky()
-  GOVUKFrontend.initAll()
-})
+// $(function () {
+//  $('.fixedsticky').fixedsticky()
+//  GOVUKFrontend.initAll()
+// })
